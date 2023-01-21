680b502de235a7c2cfc1640fb0eb7bb45275b522
fangyung <fangyung@mailer.blueoceanit.com.my>
2023-01-22 03:57:24 +0800

    Added Navbar and Footer Component

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,48 @@
+import "../styles/NavbarComponent.css";
+import aboutMe from "../images/about-me.png";
+import education from "../images/education.png";
+import experience from "../images/experience.png";
+import skills from "../images/skills.png";
+import projects from "../images/projects.png";
+import lightMode from "../images/light-mode.png";
+import darkMode from "../images/dark-mode.png";
+
+function NavbarComponent() {
+    return (
+        <div className="navbar-component-container">
+            <div className="navbar-selection-container">
+                <div className="navbar-selection">
+                    <img src={aboutMe} alt="about-me" />
+                    <span>About Me</span>
+                </div>
+                <div className="navbar-selection">
+                    <img src={education} alt="education" />
+                    <span>Education</span>
+                </div>
+                <div className="navbar-selection">
+                    <img src={experience} alt="experience" />
+                    <span>Experience</span>
+                </div>
+                <div className="navbar-selection">
+                    <img src={skills} alt="skills" />
+                    <span>Skills</span>
+                </div>
+                <div className="navbar-selection">
+                    <img src={projects} alt="projects" />
+                    <span>Projects</span>
+                </div>
+            </div>
+            <div className="light-dark-mode-container">
+                <img src={lightMode} alt="light-mode" className="light-mode" title="Light Mode" />
+                {/* <!-- Rounded switch --> */}
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <img src={darkMode} alt="dark-mode" className="dark-mode" title="Dark Mode" />
+            </div>
+        </div>
+    )
+}
+
+export default NavbarComponent;
---
+++ src/views/ProfilePage.js
@@ -0,0 +1,11 @@
+import "../styles/ProfilePage.css";
+
+function ProfilePage() {
+    return(
+        <div className="profile-page-container">
+            Profile Page Works
+        </div>
+    )
+}
+
+export default ProfilePage;
